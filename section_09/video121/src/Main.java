f783b0759ca73d3d15f1541b556d8c0e54929d32
Milindu Gomes <gomesmilindu@gmail.com>
2025-05-30 21:29:45 +0530

    java arrays

---
+++ section_09/video121/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Random;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] firstArray = getRandomArray(10);
+        System.out.println(Arrays.toString(firstArray));
+
+        Arrays.sort(firstArray);
+        System.out.println(Arrays.toString(firstArray));
+
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        Arrays.fill(secondArray, 5);
+        System.out.println(Arrays.toString(secondArray));
+
+        int[] thirdArray = new int[10];
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallArray));
+
+        int[] largeArray = Arrays.copyOf(thirdArray, 15);
+        System.out.println(Arrays.toString(largeArray));
+    }
+
+    private static int[] getRandomArray(int len){
+        Random random = new Random();
+        int[] newInt = new int[len];
+        for(int i = 0; i < len; i++){
+            newInt[i] = random.nextInt(100);
+        }
+        return newInt;
+
+}
+}
