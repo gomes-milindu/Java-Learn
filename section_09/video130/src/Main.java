b62a5c00224c2c6898cdb59ad901cf99a921a60d
Milindu Gomes <gomesmilindu@gmail.com>
2025-06-05 01:24:38 +0530

    video 130

---
+++ section_09/video130/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] array = new int[4][4];
+
+        array[0] = new int[]{1,2,3,4};
+        System.out.println(Arrays.deepToString(array));
+
+        array[1] = new int[]{10,20,30,40};
+        System.out.println(Arrays.deepToString(array));
+
+        // two dimentional arrays balagnna
+        
+    }
+}
